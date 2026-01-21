b20caf9d605b48ace01382dc4c78cc53a36bfdbf
IT Gesit <it.operation@gesit.co.id>
2026-01-21 16:32:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#BC9C33",
+                "navy-deep": "#103065",
+            },
+            fontFamily: {
+                display: ["Crimson Text", "serif"],
+                body: ["Lato", "sans-serif"],
+                serif: ["Crimson Text", "serif"],
+                sans: ["Lato", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+}
